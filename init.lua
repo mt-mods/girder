a3153bb8eeb828f7ed8c5d761719334fa879878e
unknown <24964441+wsor4035@users.noreply.github.com>
2022-09-20 20:44:13 -0400

    initial commit

---
+++ init.lua
@@ -0,0 +1,27 @@
+minetest.register_node("girder:beam", {
+    description = "beam",
+    drawtype="mesh",
+    paramtype="light",
+    paramtype2="facedir",
+    mesh="girder_beam.obj",
+    tiles = {"[combine:16x16^[noalpha^[colorize:grey"},
+    groups={oddly_breakable_by_hand=3}
+})
+minetest.register_node("girder:girder_side", {
+    description = "beam",
+    drawtype="mesh",
+    paramtype="light",
+    paramtype2="facedir",
+    mesh="girder_girder_side.obj",
+    tiles = {"[combine:16x16^[noalpha^[colorize:grey"},
+    groups={oddly_breakable_by_hand=3}
+})
+minetest.register_node("girder:plate", {
+    description = "beam",
+    drawtype="mesh",
+    paramtype="light",
+    paramtype2="facedir",
+    mesh="girder_plate.obj",
+    tiles = {"[combine:16x16^[noalpha^[colorize:grey"},
+    groups={oddly_breakable_by_hand=3}
+})
